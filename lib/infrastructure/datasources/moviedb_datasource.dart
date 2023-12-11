d121dd5645c26ea15be9861bc658b9ea458f78d8
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-10 22:48:59 -0700

    .

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -14,7 +14,7 @@ class MovieDbDatasource extends MoviesDatasource {
 
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response = await dio.get('/movie/now_playing');
+    //final response = await dio.get('/movie/now_playing');
     final List<Movie> movies = [];
 
     return movies;
