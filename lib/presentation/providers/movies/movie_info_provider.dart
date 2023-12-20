7b840c62d032ebb3e0ba66f1af0588a824b2411a
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-20 10:12:40 -0700

    MovieDetails - Cache Local

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moviepedia/domain/entities/movie.dart';
+import 'package:moviepedia/presentation/providers/providers.dart';
+
+// MovieMapNotifier es nuestro NOTIFIER
+// movieInfoProvider es nuestro PROVIDER
+
+final movieInfoProvider = StateNotifierProvider((ref) {
+  final fetchMovieInfo = ref.watch(movieRepositoryProvider).getMovieById;
+  // REGRESAMOS UNA NUEVA INSTANCIA DEL MOVIE MAP NOTIFIER
+  return MovieMapNotifier(getMovie: fetchMovieInfo);
+});
+
+/* 
+  Como va a lucir el StateNotifier ES NUESTRO state
+  {
+    '505642': Movie(),
+    '505643': Movie(),
+    '505644': Movie(),
+    '505645': Movie(),
+  }
+*/
+
+// ESPERAR FUNCION QUE REGRESE ALGO (ESTO SOLO ES LA DEFINICION)
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  final GetMovieCallback getMovie;
+
+  // LO INICIALIZAMOS COMO UN MAPA VACIO
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) return;
+
+    final movie = await getMovie(movieId);
+
+    // HACER EL SPREAD PARA GENERAR UN NUEVO ESTADO
+    state = {...state, movieId: movie};
+  }
+}
