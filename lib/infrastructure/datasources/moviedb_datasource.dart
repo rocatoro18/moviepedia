e99366344b04424599f4220fc94113dc25349cd7
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-23 11:24:29 -0700

    Datasource y repository search movie

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -74,4 +74,13 @@ class MovieDbDatasource extends MoviesDatasource {
 
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    final response =
+        await dio.get('/search/movie', queryParameters: {'query': query});
+
+    // PROCESAMIENTO
+    return _jsonToMovies(response.data);
+  }
 }
