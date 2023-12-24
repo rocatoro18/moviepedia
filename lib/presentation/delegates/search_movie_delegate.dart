9a15781ac9274056d590c4fb62b78e5a27770df6
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-23 18:45:33 -0700

    Search Delegate Leading & Actions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,19 +1,30 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:moviepedia/domain/entities/movie.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   @override
   String get searchFieldLabel => 'Buscar película';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
+    //print('query: $query');
+
     return [
-      const Text('buildActions'),
+      //if (query.isNotEmpty)
+      FadeIn(
+          animate: query.isNotEmpty,
+          duration: const Duration(milliseconds: 200),
+          child: IconButton(
+              onPressed: () => query = '', icon: const Icon(Icons.clear))),
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
