af3f7d3fc5387117146b35ac2fa020a8bb5f5b83
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-25 22:58:20 -0700

    Mostrar peliculas en la busqueda

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -50,10 +50,54 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
               itemCount: movies.length,
               itemBuilder: (context, index) {
                 final movie = movies[index];
-                return ListTile(
-                  title: Text(movie.title),
-                );
+                return _MovieItem(movie: movie);
               });
         });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          // Image
+          SizedBox(
+            width: size.width * 0.20,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                loadingBuilder: (context, child, loadingProgress) =>
+                    FadeIn(child: child),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          // Description
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(movie.title, style: textStyles.titleMedium),
+                (movie.overview.length > 100)
+                    ? Text('${movie.overview.substring(0, 100)}...')
+                    : Text(movie.overview)
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
