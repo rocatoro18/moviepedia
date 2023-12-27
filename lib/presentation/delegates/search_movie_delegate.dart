7ede9a8742fd650aae9fa1d53089503e2cf0b7e0
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-26 23:16:13 -0700

    Debounce manual

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:moviepedia/config/helpers/human_formats.dart';
@@ -8,8 +10,23 @@ typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
 
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
   final SearchMoviesCallback searchMovies;
+  // BROADCAST = SIRVE PARA TENER MULTIPLES LISTENERS
+  StreamController<List<Movie>> debouncedMovies = StreamController.broadcast();
+  // TIMER ME PERMITE A MI DETERMINAR UN PERIODO DE TIEMPO Y TAMBIEN PERMITE LIMPIARLO Y CANCELARLO
+  Timer? _debounceTimer;
 
   SearchMovieDelegate({required this.searchMovies});
+
+  void _onQueryChanged(String query) {
+    print('Query String cambio');
+    // SE ESTA LIMPIANDO CADA VEZ QUE LA PERSONA ESCRIBE ALGO SI ESTA ACTIVO LIMPIAR EL DEBOUNCE TIMER
+    if (_debounceTimer?.isActive ?? false) _debounceTimer!.cancel();
+    // CUANDO SE DEJA DE ESCRIBIR POR 500 MILESIMAS SE EJECUTA EL CALLBACK
+    _debounceTimer = Timer(const Duration(milliseconds: 500), () {
+      //TODO: Buscar peliculas y emitir al stream
+      print('Buscando peliculas');
+    });
+  }
 
   @override
   String get searchFieldLabel => 'Buscar película';
@@ -42,9 +59,12 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-        future: searchMovies(query),
+    _onQueryChanged(query);
+    return StreamBuilder(
+        //future: searchMovies(query),
+        stream: debouncedMovies.stream,
         builder: (context, snapshot) {
+          //print('Realizando Petición');
           final movies = snapshot.data ?? [];
 
           return ListView.builder(
