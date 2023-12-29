c33e01e644e5854dbd82523c7a0401dd7bf31677
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-28 22:09:13 -0700

    Configuracion del Router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,11 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:moviepedia/presentation/screens/screens.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
+final appRouter = GoRouter(initialLocation: '/home/0', routes: [
   GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final pageIndex = state.pathParameters['page'] ?? 0;
+        return HomeScreen(pageIndex: int.parse(pageIndex.toString()));
+      },
       routes: [
         // RUTAS HIJAS
         GoRoute(
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:moviepedia/presentation/views/movies/favorites_view.dart';
 import 'package:moviepedia/presentation/views/movies/home_view.dart';
 import 'package:moviepedia/presentation/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
 
   static const name = 'home-screen';
+  final int pageIndex;
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(), // CATEGORIAS
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeView(),
+    return Scaffold(
+      // WIDGET PARA PRESERVAR EL ESTADO IndexedStack
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
+      ),
       bottomNavigationBar: CustomBottomNavigation(),
     );
   }
