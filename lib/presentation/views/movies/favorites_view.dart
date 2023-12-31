f729626765605e3609224fdf455fca0aac0e0214
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-31 00:15:08 -0700

    Mostrar mensaje si no hay favoritos

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:moviepedia/presentation/providers/providers.dart';
 
 // init
@@ -44,6 +45,34 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
 
     // CONVERTIR EL MAPA DE FAVORITE MOVIES A LIST
     final favoriteMoviesList = favoriteMoviesMap.values.toList();
+
+    if (favoriteMoviesList.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.favorite_outline_sharp,
+            size: 60,
+            color: colors.primary,
+          ),
+          Text(
+            'Ohhh noo!',
+            style: TextStyle(fontSize: 30, color: colors.primary),
+          ),
+          const Text(
+            'No tienes peliculas favoritas',
+            style: TextStyle(fontSize: 20, color: Colors.black45),
+          ),
+          const SizedBox(height: 20),
+          FilledButton.tonal(
+              onPressed: () => context.go('/home/0'),
+              child: const Text('Empieza a buscar'))
+        ],
+      ));
+    }
 
     return Scaffold(
       body:
