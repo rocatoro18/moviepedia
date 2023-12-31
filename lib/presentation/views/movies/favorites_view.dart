95d2f73a2d32707427a61adfc5a4f1a27311cef7
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-12-30 21:59:21 -0700

    Mostrar texto peliculas favoritas

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moviepedia/presentation/providers/providers.dart';
 
-class FavoritesView extends StatelessWidget {
+// init
+// solo las primeras 10
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    // ESTE ES EL PUENTE, ESTE READ DEL PROVIDER VA A MANDAR LLAMAR LA SIGUIENTE PAGINA
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // OBTENER EL MAPA DE FAVORITE MOVIES
+    final favoriteMoviesMap = ref.watch(favoriteMoviesProvider);
+
+    // CONVERTIR EL MAPA DE FAVORITE MOVIES A LIST
+    final favoriteMoviesList = favoriteMoviesMap.values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
-      ),
+      body: ListView.builder(
+          itemCount: favoriteMoviesList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(favoriteMoviesList[index].title),
+            );
+          }),
     );
   }
 }
